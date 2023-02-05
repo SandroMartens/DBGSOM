f38fb2e3d128c10e30f1e64a04c492f66ca6bc32
SandroMartens <sandro.martens@web.de>
2023-02-05 15:19:02 +0100

    updates user_guide.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,6 +11,7 @@ import sys
 import pathlib
 
 sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("dbgsom"))
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 
 
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -14,6 +14,7 @@ User Guide
    :maxdepth: 2
    :caption: Contents:
 
+   user_guide
    api.rst
 
 
