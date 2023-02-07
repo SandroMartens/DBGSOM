d5e0da70f3b0c6eeade120569e3e0b33e4088c60
SandroMartens <sandro.martens@web.de>
2023-02-07 23:07:16 +0100

    created readthedocs.yaml

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 from setuptools import setup
 
-with open("README.md", 'r') as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-   name='dbgsom',
-   version='1.0.0',
-   description='A directed batch growing approach to enhance the topology preservation of self-organizing maps ',
-   long_description=long_description,
-   author='Sandro Martens',
-   author_email='sandro.martens@web.de',
-   license="MIT",
-   packages=['dbgsom'],
-   install_requires=['numpy', 'networkx', "scipy", "tqdm", "scikit-learn", "pynndescent"],
+    name="dbgsom",
+    version="1.0.0",
+    description="A directed batch growing approach to enhance the topology preservation of self-organizing maps ",
+    long_description=long_description,
+    author="Sandro Martens",
+    author_email="sandro.martens@web.de",
+    license="MIT",
+    packages=["dbgsom"],
+    install_requires=[
+        "numpy",
+        "networkx",
+        "scipy",
+        "tqdm",
+        "scikit-learn",
+        "pynndescent",
+    ],
 )
