cd69d258494cde1c455a489669f90722de1b1f05
Sandro Martens <sandro.martens@web.de>
2026-06-04 16:15:36 +0200

    push profiling file

---
+++ benchmarks/profile_training.py
@@ -0,0 +1,29 @@
+from line_profiler import LineProfiler
+from sklearn.datasets import fetch_openml
+from sklearn.preprocessing import StandardScaler
+
+from dbgsom.BaseSom import BaseSom
+from dbgsom.SomVQ import SomVQ
+
+fmnist = fetch_openml("Fashion-MNIST", version=1, as_frame=False, parser="auto")
+X = StandardScaler().fit_transform(fmnist.data[:10_000].astype(float))
+y = fmnist.target[:10_000]
+
+som = SomVQ(
+    n_iter=500,
+    spreading_factor=0.8,
+    max_neurons=100,
+    sigma_end=1,
+    random_state=42,
+)
+
+profiler = LineProfiler()
+for method in [
+    BaseSom._grow_som,
+    BaseSom._get_winning_neurons,
+    BaseSom._update_weights,
+]:
+    profiler.add_function(method)
+
+profiler.runcall(som.fit, X)
+profiler.print_stats(output_unit=1e-3)  # times in milliseconds
